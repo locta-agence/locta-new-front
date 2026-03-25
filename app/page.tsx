9b49d2354417fb56416c20ce0628a6283bdd6023
johanpoyet <jojopoy2003@gmail.com>
2026-03-25 15:11:32 +0100

    refacto: (creato componento socialsection)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ScrollGallery from "./components/ScrollGallery";
+import SocialSection from "./components/SocialSection";
 
 export default function Home() {
   return (
@@ -238,60 +239,7 @@ export default function Home() {
           />
         </div>
       </div>
-      {/* Social media section */}
-      <div style={{ display: "flex", width: "100%", padding: "120px var(--Margin-margin, 24px)", flexDirection: "column", justifyContent: "flex-end", alignItems: "stretch", gap: "20px" }}>
-        <div style={{ display: "flex", alignItems: "center", gap: "10px", alignSelf: "stretch" }}>
-
-          {/* Small image standalone */}
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/beeb9bba61fc7e58eeabf6a446cdc5f64b22af7b.png) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-
-          {/* Instagram group */}
-          <div style={{ display: "flex", height: "266.274px", flexDirection: "column", justifyContent: "flex-end", alignItems: "center", gap: "16px" }}>
-            <div style={{ display: "flex", height: "25px", justifyContent: "center", alignItems: "center", gap: "8px", flexShrink: 0 }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style={{ width: "16px", height: "16px", aspectRatio: "1/1" }}>
-                <path d="M8 1.44103C10.1387 1.44103 10.3903 1.44675 11.2366 1.48678C12.0143 1.52109 12.4432 1.65261 12.7234 1.76126C13.0951 1.90422 13.3638 2.08149 13.644 2.36169C13.9242 2.64189 14.0958 2.91065 14.2445 3.28235C14.3531 3.56255 14.4846 3.9857 14.5189 4.76912C14.559 5.61544 14.5647 5.86705 14.5647 8.00572C14.5647 10.1444 14.559 10.396 14.5189 11.2423C14.4846 12.02 14.3531 12.4489 14.2445 12.7291C14.1015 13.1008 13.9242 13.3696 13.644 13.6498C13.3638 13.93 13.0951 14.1015 12.7234 14.2502C12.4432 14.3588 12.02 14.4904 11.2366 14.5247C10.3903 14.5647 10.1387 14.5704 8 14.5704C5.86133 14.5704 5.60972 14.5647 4.7634 14.5247C3.9857 14.4904 3.55683 14.3588 3.27663 14.2502C2.90493 14.1072 2.63617 13.93 2.35597 13.6498C2.07577 13.3696 1.90422 13.1008 1.75554 12.7291C1.64689 12.4489 1.51537 12.0257 1.48106 11.2423C1.44103 10.396 1.43531 10.1444 1.43531 8.00572C1.43531 5.86705 1.44103 5.61544 1.48106 4.76912C1.51537 3.99142 1.64689 3.56255 1.75554 3.28235C1.8985 2.91065 2.07577 2.64189 2.35597 2.36169C2.63617 2.08149 2.90493 1.90994 3.27663 1.76126C3.55683 1.65261 3.97999 1.52109 4.7634 1.48678C5.60972 1.44675 5.86133 1.44103 8 1.44103ZM8 0C5.82702 0 5.55254 0.0114378 4.7005 0.045748C3.84846 0.0857766 3.26519 0.217298 2.75625 0.417441C2.23016 0.623302 1.78413 0.897784 1.3381 1.3381C0.892066 1.78413 0.617584 2.23016 0.417441 2.75625C0.217298 3.26519 0.0857756 3.84846 0.045747 4.7005C0.00571839 5.55254 0 5.82702 0 8C0 10.173 0.0114368 10.4475 0.045747 11.2995C0.0857756 12.1515 0.217298 12.7348 0.417441 13.2437C0.623302 13.7698 0.897784 14.2159 1.3381 14.6619C1.78413 15.1079 2.23016 15.3824 2.75625 15.5826C3.26519 15.7827 3.84846 15.9142 4.7005 15.9543C5.55254 15.9943 5.82702 16 8 16C10.173 16 10.4475 15.9886 11.2995 15.9543C12.1515 15.9142 12.7348 15.7827 13.2437 15.5826C13.7698 15.3767 14.2159 15.1022 14.6619 14.6619C15.1079 14.2159 15.3824 13.7698 15.5826 13.2437C15.7827 12.7348 15.9142 12.1515 15.9543 11.2995C15.9943 10.4475 16 10.173 16 8C16 5.82702 15.9886 5.55254 15.9543 4.7005C15.9142 3.84846 15.7827 3.26519 15.5826 2.75625C15.3767 2.23016 15.1022 1.78413 14.6619 1.3381C14.2159 0.892066 13.7698 0.617584 13.2437 0.417441C12.7348 0.217298 12.1515 0.0857766 11.2995 0.045748C10.4475 0.00571942 10.173 0 8 0Z" fill="black"/>
-                <path d="M7.99966 3.89421C5.72947 3.89421 3.88816 5.73553 3.88816 8.00572C3.88816 10.2759 5.72947 12.1172 7.99966 12.1172C10.2699 12.1172 12.1112 10.2759 12.1112 8.00572C12.1112 5.73553 10.2699 3.89421 7.99966 3.89421ZM7.99966 10.6762C6.52433 10.6762 5.32919 9.48106 5.32919 8.00572C5.32919 6.53038 6.52433 5.33524 7.99966 5.33524C9.475 5.33524 10.6701 6.53038 10.6701 8.00572C10.6701 9.48106 9.475 10.6762 7.99966 10.6762Z" fill="black"/>
-                <path d="M12.2712 4.69478C12.8018 4.69478 13.2319 4.26467 13.2319 3.7341C13.2319 3.20352 12.8018 2.77341 12.2712 2.77341C11.7407 2.77341 11.3106 3.20352 11.3106 3.7341C11.3106 4.26467 11.7407 4.69478 12.2712 4.69478Z" fill="black"/>
-              </svg>
-              <span style={{ textAlign: "center", fontFamily: 'var(--Heading, "Armin Soft")', fontSize: "var(--Heading-H4, 18px)", fontWeight: 600, lineHeight: "100%", textTransform: "uppercase", color: "#000" }}>@LO.CTA</span>
-            </div>
-            <div style={{ display: "flex", width: "200px", height: "266.274px", padding: "24px var(--Margin-margin, 24px)", justifyContent: "center", alignItems: "center", gap: "12px", flexShrink: 0, aspectRatio: "169/225", background: "url(/images/img-21.png) lightgray -18.678px 0px / 118.678% 110.293% no-repeat" }} />
-          </div>
-
-          {/* 2 small images */}
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/1bf71634184186caa805f3c32ce4e9bbc631740c.jpg) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/134f8f4eb69029543e7dce8d7ab181d3b98565d6-1.jpg) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-
-          {/* LinkedIn group */}
-          <div style={{ display: "flex", height: "266.274px", flexDirection: "column", justifyContent: "flex-end", alignItems: "center", gap: "16px" }}>
-            <div style={{ display: "flex", height: "25px", justifyContent: "center", alignItems: "center", gap: "8px", flexShrink: 0 }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style={{ width: "16px", height: "16px" }}>
-                <path d="M3.58052 15.993H0.259765V5.31225H3.58052V15.993ZM1.92365 3.85068C0.863538 3.85068 0 2.98638 0 1.92534C0 0.864294 0.856517 0 1.92365 0C2.99079 0 3.8473 0.864294 3.8473 1.92534C3.8473 2.98638 2.98377 3.85068 1.92365 3.85068ZM16 15.993H12.6863V10.8002C12.6863 9.56346 12.6652 7.96838 10.9592 7.96838C9.25318 7.96838 8.97236 9.31752 8.97236 10.7088V15.993H5.65862V5.31225H8.83897V6.77382H8.88109C9.32339 5.93764 10.4046 5.05226 12.0193 5.05226C15.3752 5.05226 15.993 7.2657 15.993 10.1397V16L16 15.993Z" fill="black"/>
-              </svg>
-              <span style={{ textAlign: "center", fontFamily: 'var(--Heading, "Armin Soft")', fontSize: "var(--Heading-H4, 18px)", fontWeight: 600, lineHeight: "100%", textTransform: "uppercase", color: "#000" }}>LOCTA</span>
-            </div>
-            <div style={{ display: "flex", width: "200px", height: "266.274px", padding: "24px var(--Margin-margin, 24px)", justifyContent: "center", alignItems: "center", gap: "12px", flexShrink: 0, aspectRatio: "169/225", background: "url(/images/source_imgi_69_442171572_983153753516259_113050789989027832_n.jpg) lightgray -18.678px 0px / 118.678% 110.293% no-repeat" }} />
-          </div>
-
-          {/* Small image before TikTok */}
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/134f8f4eb69029543e7dce8d7ab181d3b98565d6-1.jpg) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-
-          {/* TikTok group */}
-          <div style={{ display: "flex", height: "266.274px", flexDirection: "column", justifyContent: "flex-end", alignItems: "center", gap: "16px" }}>
-            <div style={{ display: "flex", height: "25px", justifyContent: "center", alignItems: "center", gap: "8px", flexShrink: 0 }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" fill="none" style={{ width: "14px", height: "16px" }}>
-                <path d="M13.6922 6.44306C13.5652 6.4541 13.4327 6.4541 13.3057 6.44306C11.8647 6.44306 10.5176 5.71981 9.72809 4.5107V11.136C9.72809 13.8247 7.55279 16 4.86404 16C2.17529 16 0 13.8247 0 11.136C0 8.4472 2.17529 6.27191 4.86404 6.27191H5.18979V8.69565C3.83713 8.48033 2.56177 9.40235 2.34645 10.755C2.13112 12.1077 3.05314 13.383 4.40579 13.5983C4.50517 13.6149 4.59903 13.6259 4.69841 13.6259H4.89717C6.27191 13.6259 7.39821 12.5383 7.45342 11.1691L7.49206 0H9.7833C10.0041 2.05383 11.6605 3.64941 13.7198 3.804L13.6922 3.78192V6.44306Z" fill="black"/>
-              </svg>
-              <span style={{ textAlign: "center", fontFamily: 'var(--Heading, "Armin Soft")', fontSize: "var(--Heading-H4, 18px)", fontWeight: 600, lineHeight: "100%", textTransform: "uppercase", color: "#000" }}>LO.CTA</span>
-            </div>
-            <div style={{ display: "flex", width: "200px", height: "266.274px", padding: "24px var(--Margin-margin, 24px)", justifyContent: "center", alignItems: "center", gap: "12px", flexShrink: 0, aspectRatio: "169/225", background: "url(/images/134f8f4eb69029543e7dce8d7ab181d3b98565d6.jpg) lightgray -18.678px 0px / 118.678% 110.293% no-repeat" }} />
-          </div>
-
-          {/* 2 small images */}
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/beeb9bba61fc7e58eeabf6a446cdc5f64b22af7b.png) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-          <div style={{ display: "flex", height: "175px", padding: "0px", justifyContent: "center", alignItems: "center", gap: "12px", flex: "1 0 0", aspectRatio: "118.67/157.99", background: "url(/images/img-21.png) lightgray -24.822px -13.734px / 141.834% 131.814% no-repeat" }} />
-        </div>
-      </div>
+      <SocialSection />
     </div>
   );
 }
