dab0d3cc74f4dad7a3b7a58a499540aa5ebc2ff2
ANome1 <anomenafitia@gmail.com>
2026-04-15 15:40:55 +0200

    pading fix

--- app/media/partenariat/page.tsx
+++ app/media/partenariat/page.tsx
@@ -7,14 +7,15 @@ import PartnersGrid from "@/app/components/PartnersGrid";
 
 export default function Page(){
     return (
-    <main className="p-[40px]">
-      <div className="text-center px-2 py-12 flex flex-col gap-4 md:gap-6">
+    <main>
+      <div className="p-[40px]">
+      <div className="text-center flex flex-col gap-4 md:gap-6">
         <h1 className="text-4xl lg:text-6xl font-semibold uppercase leading-tight break-words">Les partenaires Locta</h1>
         <p className="text-base">Envie de devenir partenaire ?</p>
         <Link href="/media/contact" className="inline-block w-fit self-center font-bold underline text-sm md:text-base">Nous contacter</Link>
       </div>
 
-      <section className="w-full px-2 pb-2 pt-2">
+      <section className="w-full pb-[24px] pt-[24px]">
         <div
           className="relative w-full overflow-hidden"
           style={{ height: "500px", maxHeight: "550px" }}
@@ -33,8 +34,8 @@ export default function Page(){
       <section className="flex flex-col">
         <PartnersGrid />
       </section>
+      </div>
       <SocialSection />
-
     </main>
     );
 }
